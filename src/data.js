5f1a3ad9575bd926f88e97a7529cc24f029c7170
Noha Hamed <106745773+Noha-5@users.noreply.github.com>
2024-08-14 23:47:55 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,31 +3,31 @@ export default [
         id: 1,
         img: "https://noha-5.github.io/react-travel-journal/images/20240814_211804.jpg",
         country: {
-            name: "Saudi Arabia",
-            place: "Jeddah Fakieh Aquarium"
+            name: "Jeddah",
+            place: "Fakieh Aquarium"
         },
         date: "24 Aug, 2018",
         details: "Fakieh Aquarium is the only aquarium for the public in Saudi Arabia and offers education and entertainment by presenting the wonders of the underwater environment of the Red Sea and marvels brought from other seas and oceans around the world."
     },
     {
         id: 2,
-        img: "https://source.unsplash.com/JmuyB_LibRo",
+        img: "https://noha-5.github.io/react-travel-journal/images/namar.jpg",
         country: {
-            name: "australia",
-            place: "Sydney Opera House"
+            name: "Riyadh",
+            place: "Wadi Namar"
         },
-        date: "27 May, 2021 - 8 Jun, 2021",
-        details: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+        date: "8 Jun, 2018",
+        details: "Wadi Namar Waterfall is a desert oasis with a lake, a waterfall and greenery in Wadi Hanifah, Riyadh. It is a popular spot for picnics, barbeques, sports and night views."
     },
     {
         id: 3,
-        img: "https://source.unsplash.com/3PeSjpLVtLg",
+        img: "https://noha-5.github.io/react-travel-journal/images/zoo.jpg",
         country: {
-            name: "norway",
-            place: "Geirangerfjord"
+            name: "Riyadh",
+            place: "Zoo"
         },
-        date: "01 Oct, 2021 - 18 Nov, 2021",
-        details: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+        date: "25 Nov, 2022",
+        details: "Riyadh Zoo, a sprawling animal sanctuary in the heart of Saudi Arabia’s capital, is a haven for animal lovers and families. As one of the most prominent places to visit in Riyadh, the zoo offers a unique blend of wildlife, education, and fun."
     }
 
 ]
